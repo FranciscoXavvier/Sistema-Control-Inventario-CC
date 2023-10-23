23f5b2e8e62f5fc22afd8d2a915103381a476932
AaronI45 <zs20015694@estudiantes.uv.mx>
2023-10-23 04:10:40 -0600

    Recuperar aplicaciones de bd

--- src/businesslogic/SistemaHWSW.java
+++ src/businesslogic/SistemaHWSW.java
@@ -5,11 +5,14 @@
  */
 package businesslogic;
 
+import java.util.ArrayList;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import sistemahwsw.dao.AplicacionDAO;
+import sistemahwsw.pojo.Aplicacion;
 
 /**
  *
@@ -19,7 +22,12 @@ public class SistemaHWSW extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/sistemahwsw/vistas/FXMLDocument.fxml"));
+        ArrayList<Aplicacion> aplicaciones = AplicacionDAO.obtenerAplicaciones();
+        for (Aplicacion app : aplicaciones){
+            System.out.println(app.getNombre());
+        }
+        
+        Parent root = FXMLLoader.load(getClass().getResource("/sistemahwsw/vistas/FXMLmenuPrincipal.fxml"));
         
         Scene scene = new Scene(root);
         
